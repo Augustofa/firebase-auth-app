6b906cc5517870175759c30e39128864b505bd3d
Augustofa <augusto.fer.arantes@gmail.com>
2025-09-12 16:38:50 -0300

    added linked activities

--- app/src/main/java/me/augusto/firebaseauthapp/RegisterActivity.kt
+++ app/src/main/java/me/augusto/firebaseauthapp/RegisterActivity.kt
@@ -1,6 +1,5 @@
 package me.augusto.firebaseauthapp
 
-import android.content.Intent
 import android.os.Bundle
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
@@ -10,25 +9,18 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.firebase.auth.FirebaseAuth
 
-class MainActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        setContentView(R.layout.activity_register)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.register)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-
-        findViewById<TextView>(R.id.signupText).setOnClickListener { redirectToRegisterPage() }
-    }
-
-    fun redirectToRegisterPage() {
-        val intent = Intent(this, RegisterActivity::class.java)
-        startActivity(intent)
     }
 }
